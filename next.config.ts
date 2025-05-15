3d21ca31e8d447e84fcfe02b5b5c0d981067dcc7
ArthSmit <111189998+ArthSmit@users.noreply.github.com>
2025-05-15 21:22:19 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // ← Это ключевая строка для статического экспорта
   typescript: {
     ignoreBuildErrors: true,
   },
