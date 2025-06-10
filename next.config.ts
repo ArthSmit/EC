791e5a122d24bfd83269dd230019adc421bfb7ed
ArthSmit <111189998+ArthSmit@users.noreply.github.com>
2025-06-11 01:16:53 +0300

    Fix Application Issues

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,10 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  output: 'export', // ← Это ключевая строка для статического экспорта
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
